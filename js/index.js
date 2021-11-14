8197547da6a41d3aedf1e5aba030a0cfd94663c7
trickkyricky <rickygyoungjr@gmail.com>
2021-11-13 19:02:46 -0600

    Hangman startup

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const animals = ['llama','turtle','sea lion', 'dog', 'cat', 'lion', 'giraffe','fish', 'eagle', 'falcon', 'raven','spider', 'racoon', ];
+const food = ['burger', 'steak', 'tacos', 'french fries', 'hot dogs', 'nachos', 'sandwiches', 'donuts'];
+const musicArtist = ['bruno mars','imagine dragons', 'the script','keshi','coldplay'];
+
+const menuBar = document.querySelector('.menu');
+
+menuBar.addEventListener('click', () => {
+        
+})
